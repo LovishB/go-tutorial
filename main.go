25648119d442b0d244a20da4017327cfaa021d0d
Lovish Badlani <lovish@dxsale.network>
2025-04-05 02:02:52 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main // compiler will look for the main package to start the program
+
+import "fmt" // fmt package is form go standard library
+
+func main() {
+	fmt.Println("Hello, Go!")
+}
